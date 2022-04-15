cd6594814cc0464af09ba77676329ebb1117bd54
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-15 23:05:15 +0600

    Create and_or_shorthand.js

---
+++ and_or_shorthand.js
@@ -0,0 +1,14 @@
+
+//or operator
+
+var name = ``
+
+var fullName = name || `S M Shakib`
+console.log(fullName)
+
+//and operator
+
+var age = `22`
+
+var myAge = age || `years old.`
+console.log(myAge)
