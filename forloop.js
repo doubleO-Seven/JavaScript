d40ff460a8527a448a816eb95b98914509217007
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-15 23:21:30 +0600

    a

--- forloop.js
+++ forloop.js
@@ -1,5 +1,11 @@
 
 // simple loop
 for(var i =0 ; i < 10 ; i++){
-    console.log(`S M Shakib`)
+    console.log((i + 1)  + ` S M Shakib`)
+}
+
+//print even
+for(var i=0; i<=100; i++){
+    if(i%2 === 0)
+        console.log(i)
 }
