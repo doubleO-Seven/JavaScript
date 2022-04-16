40ebc05e8ec067e1311d1206c5e4302368f1b74d
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-16 14:16:52 +0600

    1st commit

---
+++ object.js
@@ -0,0 +1,23 @@
+var point = {}
+point.x = 20
+console.log(point)
+
+//object literal
+var a = {
+    x: 10,
+    y: 20
+}
+
+a.z = 100
+console.log(a)
+
+//obj constructor
+
+var obj = Object()
+obj.a = 14
+
+console.log(obj)
+
+var obj2 = new Object()
+obj2.b = 78
+console.log(obj2)
