3b36faf0f994003121d90dcc1ce4c01b60bb151b
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-16 14:29:29 +0600

    Update object.js

--- object.js
+++ object.js
@@ -49,3 +49,21 @@ console.log(a)
 
 delete a.x
 console.log(a)
+
+//comapare 2 obj
+var obj1 = {
+    a: 10,
+    b: 20
+}
+
+var obj2 = {
+    a: 10,
+    b: 20
+}
+console.log(obj1 === obj2)
+
+if(obj1.a === obj2.a && obj1.b === obj2.b){
+    console.log(true)
+} else {
+    console.log(false)
+}
