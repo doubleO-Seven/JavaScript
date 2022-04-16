60dad3d5317da36f229b47c3550c165230d0e36f
S M Shakib Hossain <smsakib2403@gmail.com>
2022-04-16 13:53:04 +0600

    Update array.js

--- array.js
+++ array.js
@@ -17,3 +17,17 @@ for(var i=0; i<arr.length; i++)
 {
     console.log(arr[i])
 }
+
+//search value in array
+var arr = [1,2,3,4,5,6,7]
+
+var find = 1
+for(var i=0; i<arr.length; i++){
+    if(arr[i] === find){
+        console.log(`Data found at: ` + i)
+            break;}
+    else{
+        console.log(`Data is not found.`)
+        break;
+    }
+}
